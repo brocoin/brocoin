8375dae2a73cda3705bd464ae87558bd3903cbae
Chris Bielinski <cbielinski@acorns.com>
2017-09-10 10:23:03 -0700

    litecoin => brocoin v2

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -9,10 +9,10 @@ BITCOINCLI=${BITCOINCLI:-$SRCDIR/brocoin-cli}
 BITCOINTX=${BITCOINTX:-$SRCDIR/brocoin-tx}
 BITCOINQT=${BITCOINQT:-$SRCDIR/qt/brocoin-qt}
 
-[ ! -x $BITCOIND ] && echo "$LITECOIND not found or not executable." && exit 1
+[ ! -x $BITCOIND ] && echo "$BROCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+BTCVER=($($BROCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
--- doc/tor.md
+++ doc/tor.md
@@ -1,4 +1,4 @@
-TOR SUPPORT IN LITECOIN
+TOR SUPPORT IN BROCOIN
 ======================
 
 It is possible to run Brocoin as a Tor hidden service, and connect to such services.
